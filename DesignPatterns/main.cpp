5efec111a8c846f52aefcaafd7b40953a2cd61fe
Danil Fisiuk <fisyuklol@gmail.com>
2022-04-11 21:49:08 +0300

    fix build

--- DesignPatterns/main.cpp
+++ DesignPatterns/main.cpp
@@ -1,16 +1,24 @@
 #include <iostream>
 
-#include "Factory/WindowsWidgetsFactory.hpp"
 #include "Builder/BlogPostBuilder.hpp"
+#include "Factory/LinuxWidgetsFactory.hpp"
+#include "Factory/WindowsWidgetsFactory.hpp"
 
 int main()
 {
+#ifdef _WIN32
     auto wwf = std::make_unique<WindowsWidgetsFactory>();
     wwf->createButton()->paint();
     wwf->createCheckBox()->paint();
     wwf->createField()->paint();
-    
-    auto pbp = std::make_unique<BlogPostBuilder>();
+#elif __linux__
+    auto lwf = std::make_unique<LinuxWidgetsFactory>();
+    lwf->createButton()->paint();
+    lwf->createCheckBox()->paint();
+    lwf->createField()->paint();
+#endif
+
+    auto pbp      = std::make_unique<BlogPostBuilder>();
     auto blogPost = pbp->setTitle("title")->setBody("body")->getBlogPost();
     std::cout << blogPost->body << " " << blogPost->title << std::endl;
 
