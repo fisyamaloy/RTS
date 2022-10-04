7a8f17b3703c3e6c4836d8335e7da6a7d7b2ced5
fisyamaloy <fisyuklol@gmail.com>
2022-10-04 18:41:27 +0300

    serialization added to fix ipc problem

--- IntermediateProgram/2_ReadWriteShMem/CopyingTool.cpp
+++ IntermediateProgram/2_ReadWriteShMem/CopyingTool.cpp
@@ -2,13 +2,41 @@
 
 #include <stdio.h>
 
+#include <boost/archive/text_iarchive.hpp>
+#include <boost/archive/text_oarchive.hpp>
 #include <boost/interprocess/ipc/message_queue.hpp>
 #include <boost/interprocess/mapped_region.hpp>
 #include <boost/interprocess/shared_memory_object.hpp>
+#include <boost/serialization/string.hpp>
 #include <iostream>
+#include <string>
 #include <thread>
 
 static constexpr size_t BUFFER_SIZE = 1024;
+
+struct SharedMemoryData
+{
+    std::string buffer;
+    bool        finish;
+    SharedMemoryData() : finish(false) {}
+    SharedMemoryData(char* buffer, bool isFinish) : buffer(buffer), finish(isFinish) {}
+
+    inline bool isFinished() const
+    {
+        return finish;
+    }
+
+    friend class boost::serialization::access;
+
+private:
+    template <class Archive>
+    void serialize(Archive& ar, const unsigned int version)
+    {
+        (version);
+        ar& finish;
+        ar& buffer;
+    }
+};
 
 namespace ShMemCopyingTool
 {
@@ -30,12 +58,13 @@ namespace ShMemCopyingTool
         try
         {
             boost::interprocess::shared_memory_object b{
-                create_only, _shMemName.c_str(), read_write};
-            boost::interprocess::shared_memory_object::remove(_shMemName.c_str());
+                create_only, std::string(_shMemName + "dsff").c_str(), read_write};
             return true;
         }
         catch (boost::interprocess::interprocess_exception&)
         {
+            boost::interprocess::shared_memory_object::remove(
+                std::string(_shMemName + "dsff").c_str());
         }
 
         return false;
@@ -47,16 +76,22 @@ namespace ShMemCopyingTool
 
         constexpr boost::ulong_long_type   MAX_MESSAGES = 100;
         boost::interprocess::message_queue mq(
-            create_only, _shMemName.c_str(), MAX_MESSAGES, BUFFER_SIZE);
+            create_only, _shMemName.c_str(), MAX_MESSAGES, BUFFER_SIZE + sizeof(SharedMemoryData));
 
         FileReader _fileReader;
         _fileReader.open(_sourceFileName);
 
         while (!_fileReader.isEndOfFile())
         {
-            const auto       bytesChunk = _fileReader.readNextBytesChunk(BUFFER_SIZE);
-            SharedMemoryData shMemBuf{bytesChunk, _fileReader.isEndOfFile()};
-            mq.send(&shMemBuf, sizeof(shMemBuf), 0);
+            const auto        bytesChunk = _fileReader.readNextBytesChunk(BUFFER_SIZE);
+            SharedMemoryData  shMemBuf{bytesChunk, _fileReader.isEndOfFile()};
+            
+            std::stringstream oss;
+            boost::archive::text_oarchive oa(oss);
+            oa << shMemBuf;
+            std::string serialized_string(oss.str());
+
+            mq.send(serialized_string.data(), serialized_string.size(), 0);
         }
     }
 
@@ -71,11 +106,18 @@ namespace ShMemCopyingTool
         SharedMemoryData shMemBuffer;
         while (!shMemBuffer.isFinished())
         {
-            mq.receive(&shMemBuffer, BUFFER_SIZE, recievedSize, priority);
-            _fileWriter.writeBytesChunk(shMemBuffer.buffer);
-            delete[] shMemBuffer.buffer;
-            delete shMemBuffer.finish;
+            std::stringstream iss;
+            std::string       serialized_string;
+            serialized_string.resize(BUFFER_SIZE + sizeof(SharedMemoryData));
+            mq.receive(&serialized_string[0], BUFFER_SIZE + sizeof(SharedMemoryData), recievedSize,
+                priority);
+            iss << serialized_string;
+            boost::archive::text_iarchive ia(iss);
+            ia >> shMemBuffer;
+
+            _fileWriter.writeBytesChunk(shMemBuffer.buffer.c_str());
         }
+
         boost::interprocess::message_queue::remove(_shMemName.c_str());
     }
 
--- IntermediateProgram/2_ReadWriteShMem/CopyingTool.hpp
+++ IntermediateProgram/2_ReadWriteShMem/CopyingTool.hpp
@@ -9,24 +9,6 @@
 
 namespace ShMemCopyingTool
 {
-    struct SharedMemoryData
-    {
-        char* buffer;
-        bool* finish;
-        SharedMemoryData() : buffer(nullptr), finish(nullptr) {}
-        SharedMemoryData(char* buffer, bool isFinish) : buffer(buffer), finish(new bool(isFinish))
-        {
-        }
-
-        inline bool isFinished() const
-        {
-            if (finish) 
-                return *finish;
-
-            return false;
-        }
-    };
-
     class CopyingTool final
     {
     public:
